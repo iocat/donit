831322c5832bb2f37e07e1232b5167dad86666f4
Thanh Ngo <felix.infinite@gmail.com>
2016-09-01 22:16:23 -0400

    internal: fix doc typo

--- internal/donitdoc/doc.go
+++ internal/donitdoc/doc.go
@@ -1,9 +1,9 @@
 // Package donitdoc contains document modeling for all user's data
 // Every data is stored in MongoDB in terms of documents.
 //
-// Modeling does not care about relationships and linking between data
-// Current modeling has no concern about who owns the data.
-//
+// Modeling does not care about relationships between data and who owns them.
 // Though, some model can still add additional linkages, but linkages should be
 // mainly maintained by upper level packages.
+//
+// Also check github.com/iocat/donit/internal/donitlink for more info
 package donitdoc
--- internal/donitlink/doc.go
+++ internal/donitlink/doc.go
@@ -1,4 +1,4 @@
-// Package donitlink maintains relationship between data models
+// Package donitlink maintains relationships between data models
 //
 // This package mainly deals with Cayley graph database to link data
 // model and perform efficient retrievals of graph-like data of the
