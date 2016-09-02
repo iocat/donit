aa0592ada2f95f0fe94579d331d3dc3bbf5bb722
Thanh Ngo <felix.infinite@gmail.com>
2016-09-01 22:13:14 -0400

    donitlink: fix typo

--- internal/donitlink/doc.go
+++ internal/donitlink/doc.go
@@ -1,6 +1,6 @@
-// Package donitlink maintains linking between data models
+// Package donitlink maintains relationship between data models
 //
 // This package mainly deals with Cayley graph database to link data
-// model to perform efficient retrieval of graph-like data of the
+// model and perform efficient retrievals of graph-like data of the
 // donit social network
 package donitlink
