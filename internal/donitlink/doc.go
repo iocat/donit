638653c6256364605cb07b1cf301307f4d64c3a3
Thanh Ngo <felix.infinite@gmail.com>
2016-09-01 22:11:00 -0400

    donitlink: initiate package link

---
+++ internal/donitlink/doc.go
@@ -0,0 +1,6 @@
+// Package donitlink maintains linking between data models
+//
+// This package mainly deals with Cayley graph database to link data
+// model to perform efficient retrieval of graph-like data of the
+// donit social network
+package donitlink
