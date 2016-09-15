b99e07b5a8751cc38f70ace6f6efc297d4cca98a
Thanh Ngo <felix.infinite@gmail.com>
2016-09-14 21:12:05 -0400

    handler: add achievable handlers

---
+++ handler/achievable.go
@@ -0,0 +1,124 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/iocat/donit/handler/internal/utils"
+	"github.com/iocat/donit/internal/achieving"
+	"github.com/iocat/donit/internal/achieving/validator"
+)
+
+// handler will receive a key if getResourceKey is marked true
+func decorateAchievableHandler(getResourceKey bool, handler func(achieving.Goal, string, http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	var keyGeneratorFunc func() []string
+	if getResourceKey {
+		keyGeneratorFunc = Achievable.resourceKeyNames
+	} else {
+		keyGeneratorFunc = Achievable.collectionKeyNames
+	}
+
+	// getParentResource gets the parent resource of the Achievable
+	var getParentResource = func(username string, id string) (achieving.Goal, error) {
+		user, err := store.RetrieveUser(username)
+		if err != nil {
+			return nil, err
+		}
+		gid, err := achieving.CreateID(id)
+		if err != nil {
+			return nil, err
+		}
+		goal, err := user.RetrieveGoal(gid)
+		if err != nil {
+			return nil, err
+		}
+		return goal, nil
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ids []string
+			err error
+		)
+		// get the username and the goal id
+		ids, err = utils.MuxGetParams(r, keyGeneratorFunc()...)
+		if err != nil {
+			utils.HandleError(err, w)
+			return
+		}
+		username, goalid := ids[0], ids[1]
+		goal, err := getParentResource(username, goalid)
+		if err != nil {
+			utils.HandleError(err, w)
+			return
+		}
+		// Get the resource id by request
+		var achid string
+		if getResourceKey {
+			achid = ids[2]
+		}
+		handler(goal, achid, w, r)
+	})
+}
+
+// CreateAchievable creates an achievable task
+var CreateAchievable = decorateAchievableHandler(false, createAchievable)
+
+// UpdateAchievable updates an achievable task
+var UpdateAchievable = decorateAchievableHandler(true, updateAchievable)
+
+// DeleteAchievable deletes an achievable task
+var DeleteAchievable = decorateAchievableHandler(true, deleteAchievable)
+
+// ReadAchievables reads a list of achievable tasks
+var ReadAchievables = decorateAchievableHandler(false, readAchievables)
+
+func createAchievable(goal achieving.Goal, _ string, w http.ResponseWriter, r *http.Request) {
+	ach, err := validator.Validate(r.Body, Achievable.interpreter())
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	achid, err := goal.AddAchievable(ach.(achieving.Achievable))
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	utils.WriteJSONtoHTTP(nil, w, http.StatusCreated)
+}
+
+func updateAchievable(goal achieving.Goal, key string, w http.ResponseWriter, r *http.Request) {
+	ach, err := validator.Validate(r.Body, Achievable.interpreter())
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	achid, err := achieving.CreateID(key)
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	err = goal.UpdateAchievable(ach.(achieving.Achievable), achid)
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	utils.WriteJSONtoHTTP(nil, w, http.StatusOK)
+}
+
+func deleteAchievable(goal achieving.Goal, key string, w http.ResponseWriter, _ *http.Request) {
+	achid, err := achieving.CreateID(key)
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	err = goal.RemoveAchievable(achid)
+	if err != nil {
+		utils.HandleError(err, w)
+		return
+	}
+	utils.WriteJSONtoHTTP(nil, w, http.StatusOK)
+}
+
+func readAchievables(goal achieving.Goal, _ string, w http.ResponseWriter, r *http.Request) {
+	// TODO: need retrival function from internal packages
+}
