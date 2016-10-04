dd74fa8bc04d61bb637ea406e3c2b4540bbfe7f2
Thanh Ngo <felix.infinite@gmail.com>
2016-10-03 23:17:29 -0400

    social: add doc.go

---
+++ internal/social/doc.go
@@ -0,0 +1,6 @@
+// Package social handles the socializing aspect of the
+// donit network
+// That includes
+// - Followers ( RECONSIDER !!! Should we have a follower features ? Does it matter ?)
+// - Commenting (Definitely)
+package social
